3473405cd7f5a6a2c13061ca13af5bcf9961f60b
Ardysatrio F Haroen <30514249+ardyh@users.noreply.github.com>
2018-10-05 02:40:44 +0700

    Update MatriksDriver.java

--- Tubes 1/src/MatriksDriver.java
+++ Tubes 1/src/MatriksDriver.java
@@ -2,16 +2,36 @@
 public class MatriksDriver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub         
 		Matriks M1 = new Matriks();
+                System.out.println("MENU");
+		System.out.println("1. Sistem Persamaan Linear");
+		System.out.println("2. Interpolasi Polinom");
+		System.out.println("3. Keluar");
+		
+		System.out.println("Pilih metode penyelesaian");
+		System.out.println("1. Metode eliminasi Gauss");
+		System.out.println("2. Metode eliminasi Gauss-Jordan");
 		M1.IsiMatriks();
-		M1.TulisMatriks(M1.nbaris, M1.nkolom);
-		M1.TukarBaris(2, 3);
-		M1.TulisMatriks(M1.nbaris, M1.nkolom);
-		M1.KaliBaris(1, 4);
-		M1.TulisMatriks(M1.nbaris, M1.nkolom);
-		M1.TambahBarisLain(1, 2, 2);
-		M1.TulisMatriks(M1.nbaris, M1.nkolom);
- 	}
+		
+                M1.EleminasiGauss();
+                M1.Solusi();
+               // System.out.println(M1.nilai[1]);
+              /*M1.IsiMatriksInterpolasi();
+                M1.TulisMatriks(M1.nbaris, M1.nkolom);
+                M1.InterpolasiGauss();
+                M1.TulisMatriks(M1.nbaris, M1.nkolom);
+                M1.representasiInterpolasi();
+                M1.Fx();*/
+                
+	/*	M1.IsiMatriksInterpolasi();
+                M1.TulisMatriks(M1.nbaris, M1.nkolom);
+		M1.InterpolasiGauss();
+                M1.representasiInterpolasi();
+          	M1.InterpolasiGaussJordan();
+                M1.representasiInterpolasi();
+                M1.Fx();
+ 	}*/
+}
 
 }
